1f742dcbc002391764cf96673d14a301751d268d
tanianasrin1 <tasrineva169@gmail.com>
2022-06-14 21:34:34 +0600

    footer pagw fixed commit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Banner.css'
+import bg from '../../../images/p_n.jpg'
+import { Button } from 'react-bootstrap';
 
 
 const Banner = () => {
     return (
-        <div className='backColor'>
-            <div className="container">
-                <h1 className='text-white'>I'm Web Developer</h1>
-                <h2 className='text-white'>Tania Nasrin</h2>
-            </div>
-            <div>
-                <img src="" alt="" />
-            </div>
-            
+        <div className='bgColor'>
+           <div className='d-flex container '>
+              <div className='p-5'>
+                 <h1 className='text-white'>I'm Web Developer</h1>
+                 <h4 className='text-white pb-3'>Tania Nasrin</h4>
+                 <Button variant="outline-info">My Resume</Button>
+              </div>
+              <div>
+                <img className='p-5' height="500px" src={bg} alt="" />
+              </div>
+           </div>
+       
         </div>
     );
 };
 
 export default Banner;
+
